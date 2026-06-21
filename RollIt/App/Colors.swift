b23c2cfbbaa3e7a204380bbda190fcdfcaa0fdf7
Hunter <hunterom8@proton.me>
2026-06-21 15:35:44 -0700

    Update accent color to Prestigious Golden Reel Gold

--- RollIt/App/Colors.swift
+++ RollIt/App/Colors.swift
@@ -2,7 +2,11 @@ import SwiftUI
 
 extension Color {
     public static let oledBlack = Color(red: 0, green: 0, blue: 0)
-    public static let neonAmber = Color(red: 1.0, green: 0.584, blue: 0.0) // #FF9500
+    public static let goldenReel = Color(red: 1.0, green: 0.843, blue: 0.0) // #FFD700 Gold
+    
+    // Alias to keep references in views compiling and automatically redirect to Gold
+    public static let neonAmber = goldenReel
+    
     public static let primaryText = Color.white
     public static let secondaryText = Color.white.opacity(0.6)
 }
